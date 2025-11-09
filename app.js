e3c8556fb3efeab263a82dfde6bc2e827a154c07
Abbas-developer <abbas.ghulam.hack@gmail.com>
2025-11-09 11:55:15 +0500

    Assingment completed

---
+++ app.js
@@ -0,0 +1,68 @@
+// 🔸 Assignment 1: Welcome Message
+// Objective:
+// Use prompt() to ask for the user's name.
+// Use alert() to display:
+// "Welcome, [name]!"
+// Concepts used: prompt, alert, var, concatenation
+
+
+// var userName = prompt("Please enter your name:");
+// alert("Welcome, " + userName + " to my website");
+
+
+// 🔸 Assignment 2: Age Message
+// Objective:
+// Ask the user for their name and age using prompt().
+// Log this message in the console:
+// "[name] is [age] years old."
+// Concepts used: prompt, console.log, var, concatenation
+
+
+// var age = prompt("Enter your age:");
+// alert(" hy " + userName + " you are " + age + " this year");
+
+
+// 🔸 Assignment 3: Favorite Color
+// Objective:
+// Ask the user for their favorite color.
+// Show an alert like:
+// "Wow! [color] is a nice color!"
+// Concepts used: prompt, alert, var, concatenation
+
+
+// var color = prompt("what is your favorite color? i am interested to know");
+// alert("Your favorite color is " + color + " that is a nice color");
+
+
+
+// 🔸 Assignment 4: Simple Math Sentence
+// Objective:
+// Ask the user for two numbers using prompt().
+// Show a sentence in the console like:
+
+
+// var num1 = +prompt("Enter your frist number:" );
+// var num2 = +prompt("Enter your second number:" );
+// var Result = (num1 + num2);
+// alert("The result of adding is " + Result);
+
+
+// 🔸 Assignment 5: About Me Summary
+// Objective:
+// Ask the user for their name, hobby, and country using prompt().
+// Show an alert like:
+// "Hi, I'm [name] from [country], and I love [hobby]!"
+// Concepts used: prompt, alert, var, concatenation
+
+
+// var hobby = prompt("what is your hobby Exprees it in one word");
+// alert("Your hobby is " + hobby + " that is a nice hobby");
+
+
+// var country = prompt("which country do you live in right now ?");
+// alert("Hi , i am " + userName + " from " + country + " and i love " + hobby );
+
+
+
+
+
