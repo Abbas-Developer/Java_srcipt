997d034fbde8dd072bcd24f370be79ae11734181
Abbas-developer <abbas.ghulam.hack@gmail.com>
2025-11-15 11:59:25 +0500

    assignment 2 completed

---
+++ assignment 2/app.js
@@ -0,0 +1,74 @@
+
+// -----------------------------------------------------------
+
+// 🔹 Task 1: Create a Full Address
+// Instruction:
+// Ask the user to enter their street, city, and country. Concatenate these values into one full
+// address string and display it like:
+// "Your full address is: 123 Main St, Springfield, USA"
+
+
+// var s = prompt("write your street location :" )
+// var c = prompt("Write your city name :")
+// var country = prompt("Write your country name :")
+
+// alert("This is your current address: " + s + ", " + c + ", " + country + ".")
+
+// ---------------------------------------------------------------
+
+// 🔹 Task 2: Make a Custom Username
+// Instruction:
+// Ask the user to input their first name and a favorite number. Concatenate them to create a
+// simple username like:
+// "john42"
+// Display the result with a message: "Your new username is: john42"
+
+// alert("Wellcome to username maker javascript")
+// var n = prompt("Enter your name")
+// var fn = prompt("Enter your Favorite number")
+// alert("Your Username is " + n + fn)
+
+// -------------------------------------------------------------------
+
+// 🔹 Task 3: Email Generator
+// Instruction:
+// Ask the user for their first name and last name. Combine them into an email address format
+// like:
+// "firstname.lastname@example.com"
+// Show the result.
+
+
+// var firstname = prompt("Enter your First Name: ")
+// var lastname = prompt("Enter your last name: ")
+// alert("Your email address is : " + firstname + lastname + "@gmail.com")
+
+
+// ------------------------------------------------------------------------
+
+// 🔹 Task 4: Combine Favorite Things
+// Instruction:
+// Ask the user for their favorite color, animal, and food. Create a sentence like:
+// "Your favorite things are: blue dogs and pizza!"
+
+
+
+// var color = prompt("what is your Favorite color: ")
+// var animal = prompt("What is your favorite animal: ")
+// var food = prompt("what is your favorite food:")
+// alert("Your favorite things are , " + color + ", " + animal +  ", " + food )
+
+// -----------------------------------------------------------------------------
+// 🔹 Task 5: Quote with Author
+
+// Instruction:
+// Ask the user for a quote and the author’s name. Display it in the format:
+// "The only limit is your mind." - John Doe
+// var author = prompt("what is the Author name")
+// alert("The Author name is: " + author)
+
+// ---------------------------------------------------------------------------------
+
+
+
+
+
