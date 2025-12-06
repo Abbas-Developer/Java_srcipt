652d6ad73505a9c2f10281f66aa15c89aacb6b1d
Abbas-Developer <abbas.ghulam.hack@gmail.com>
2025-12-06 14:33:30 +0500

    arrey completed

---
+++ assingment 4 if else/script.js
@@ -0,0 +1,12 @@
+
+
+// 1. Ask the user for their age. If the age is 18 or more, show an alert: "You are an
+// adult.", else show: "You are a minor."
+
+let age = +prompt("Enter you age")
+if(age >= 18){
+    alert("You are adult:")
+} 
+else {
+    alert("You are minor:")
+}
